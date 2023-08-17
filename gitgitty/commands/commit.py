99fa8e04ad29c8d51d2906ef62f979f68673472e
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2023-08-17 18:59:28 +0700

    feat: utils and commit

--- gitgitty/commands/commit.py
+++ gitgitty/commands/commit.py
@@ -1,2 +1,39 @@
+from datetime import datetime
+from .utils import utils
+import os
+
 def commit(args):
-    print('commit')
+    cwd = os.getcwd()
+    repo_path = utils.find_repository(cwd)
+
+    if repo_path is None:
+        print("not in a repository")
+        return
+    
+
+    time = datetime.now().time()
+    date = datetime.now().date()
+    msg = ""
+    if len(args) == 0:
+        msg = f"Committed at {time}-{date}"
+    else:
+        if args[0] != '-m' or len(args) > 2:
+            print("invalid command usage, syntax: 'gitgitty commit [-m <msg>]'")
+            return
+        
+        msg = args[1]
+
+
+    try:
+        repo_files = os.path.join(repo_path, "..")
+        new_snapshot_path = os.path.join(repo_path, "snapshots", str(utils.get_latest(repo_path) + 1))
+        print(repo_files, new_snapshot_path)
+        
+        utils.copy_existing_files(repo_files, new_snapshot_path)
+        utils.increment_latest(repo_path)
+        utils.set_head(repo_path, utils.get_latest(repo_path))
+        utils.append_log(repo_path, utils.get_latest(repo_path), date, time, msg)
+
+        print("commit success")
+    except Exception as e:
+        print(f"{e}\ncommit failure")
--- gitgitty/commands/utils/utils.py
+++ gitgitty/commands/utils/utils.py
@@ -4,6 +4,9 @@ import shutil
 def copy_existing_files(source, destination):
     existing_files = [f for f in os.listdir(source) if os.path.isfile(os.path.join(source, f))]
 
+    if not os.path.exists(destination):
+        os.makedirs(destination, exist_ok=False)
+
     try:
         for filename in existing_files:
             source_file_path = os.path.join(source, filename)
@@ -14,3 +17,82 @@ def copy_existing_files(source, destination):
     except PermissionError as e:
         print(f"PermissionError: {e}\nfile copy failed")
         raise e
+    except OSError as e:
+        print(f"FileNotFoundError: {e}\n file copy failed")
+        raise e
+
+
+def find_repository(start_directory):
+    for root, dirs, files in os.walk(start_directory):
+        if ".gitgitty" in dirs:
+            return os.path.join(root, ".gitgitty")
+
+    return None  # If the target folder is not found
+
+
+def get_int_file(repository_path, filename):
+    file_path = os.path.join(repository_path, filename)
+
+    try:
+        with open(file_path, "r") as file:
+            content = file.read()
+            return int(content)
+
+    except FileNotFoundError:
+        print(f"The file '{filename}' does not exist.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def write_int_file(repository_path, filename, value):
+    file_path = os.path.join(repository_path, filename)
+
+    try:
+        with open(file_path, "w") as file:
+            content = file.write(str(value))
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def get_latest(repository_path):
+    try:
+        return get_int_file(repository_path, "latest")
+    except Exception as e:
+        return -1
+
+
+def get_head(repository_path):
+    try:
+        return get_int_file(repository_path, "head")
+    except Exception as e:
+        return -1
+    
+def set_head(repository_path, value):
+    try:
+        write_int_file(repository_path, "head", value)
+    except Exception as e:
+        print("failed to set head")
+
+
+def increment_latest(repository_path):
+    try:
+        write_int_file(repository_path, "latest", get_latest(repository_path) + 1)
+    except Exception as e:
+        print("failed to set latest")
+
+
+def get_log(repository_path):
+    # todo
+    pass
+
+
+def append_log(repository_path, id, date, time, msg):
+    try:
+        with open(os.path.join(repository_path, "log"), "a") as file:
+            new_log = f"{id}, {date}, {time}, {msg}\n"
+            file.write(new_log)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
